985ee308d9d60f1ae6d03693749384d6c81c6680
ADMIN <ADMIN@ADMIN-PC>
2019-07-21 21:49:05 +0530

    First commit

---
+++ JAVASE/src/mypack/Calc.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class Calc
+{
+	public int sqr(int num)throws MyException
+	{
+		if(num<=0)
+		{
+			throw new MyException("Zero or negative not allowed");
+		}
+		return num*num;
+	}
+}
---
+++ JAVASE/src/mypack/MyException.java
@@ -0,0 +1,9 @@
+package mypack;
+
+public class MyException extends Exception
+{
+	public MyException(String mess)
+	{
+		super(mess);
+	}
+}
